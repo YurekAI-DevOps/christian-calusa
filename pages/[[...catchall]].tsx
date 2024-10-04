70288a3331e7472494fca6adaa3fe43647086a57
IWH DevOps <73498897+iwhdevops@users.noreply.github.com>
2024-10-04 18:13:19 +0200

    Update [[...catchall]].tsx

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -120,11 +120,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
           if (isCockpit(baseUrl)) {
             return response.data.map(({ uuid }: any) => uuid) || [];
           } else if (isMLS(baseUrl)) {
-            if (response?.items) {
-              return response?.items.map(({ idProject }: any) => idProject) || [];
-            } else {
-              return [ response.idProject ] || [61, 62];
-            }
+            console.log("response mls:". response);
+          
+            return response?.items.map(({ idProject }: any) => idProject) || [];
             
           } else {
             return [];
